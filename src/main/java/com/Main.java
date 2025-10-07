9c231c1218c5430099d4f3d67ad48e54a54dab96
Ralph <andrematveev57@gmail.com>
2025-10-07 19:26:46 +0300

    Написана реализация функции Main

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,5 +3,9 @@ package com;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        double Celsius = 32;
+        double Fahrenheit = 41;
+        ConvertClass.CelsiusToFahrenheit(Celsius);
+        ConvertClass.FahrenheitToCelsius(Fahrenheit);
     }
 }
