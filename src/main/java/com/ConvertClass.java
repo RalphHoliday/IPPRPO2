ce96a692018e378ebd6d32fbb97dec95e1bcbb29
Ralph <andrematveev57@gmail.com>
2025-10-07 12:54:31 +0300

    Создание функции CelsiusToFahrenheit

--- src/main/java/com/ConvertClass.java
+++ src/main/java/com/ConvertClass.java
@@ -1,4 +1,13 @@
 package com;
 
 public class ConvertClass {
+    public static double CelsiusToFahrenheit(double C) {
+        // initialising
+        double fahrenheit = 0.0;
+
+        // formula for conversion
+        fahrenheit = (C * 1.8) + 32;
+        System.out.println(" value of temperature in fahrenheit:" + fahrenheit);
+        return fahrenheit;
+    }
 }
